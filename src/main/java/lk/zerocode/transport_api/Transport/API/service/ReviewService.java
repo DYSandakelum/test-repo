ee4afc6f7f9108bf752245c186685825d5a483f9
01dinu <01dinusha@gmail.com>
2026-03-25 23:00:54 +0530

    update by id

--- src/main/java/lk/zerocode/transport_api/Transport/API/service/ReviewService.java
+++ src/main/java/lk/zerocode/transport_api/Transport/API/service/ReviewService.java
@@ -1,6 +1,7 @@
 package lk.zerocode.transport_api.Transport.API.service;
 
 import lk.zerocode.transport_api.Transport.API.controller.request.ReviewRequest;
+import lk.zerocode.transport_api.Transport.API.controller.response.ReviewResponse;
 import lk.zerocode.transport_api.Transport.API.model.Review;
 
 import java.util.List;
@@ -10,6 +11,14 @@ public interface ReviewService {
     void createReview(ReviewRequest reviewRequest);
 
     List<Review> getAllReviews();
+
+    Review findById(Long reviewId);
+
+    Review updateById(Long reviewId, ReviewRequest reviewRequest);
+
+
+
+
 
 
 }
